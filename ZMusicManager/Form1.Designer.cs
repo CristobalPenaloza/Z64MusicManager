7b82374960adc74c4006a2a5d0dc2dfbd6ca3852
Cristóbal Peñaloza <cristobal.tolin@gmail.com>
2024-03-29 22:41:59 -0300

    First commit

--- ZMusicManager/Form1.Designer.cs
+++ ZMusicManager/Form1.Designer.cs
@@ -27,13 +27,185 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.btnOpen = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutZMusicManagerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(439, 28);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.btnOpen,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// newToolStripMenuItem
+			// 
+			this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+			this.newToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+			this.newToolStripMenuItem.Text = "New";
+			// 
+			// btnOpen
+			// 
+			this.btnOpen.Name = "btnOpen";
+			this.btnOpen.Size = new System.Drawing.Size(144, 26);
+			this.btnOpen.Text = "Open...";
+			this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+			// 
+			// saveToolStripMenuItem
+			// 
+			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+			this.saveToolStripMenuItem.Text = "Save";
+			// 
+			// saveAsToolStripMenuItem
+			// 
+			this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+			this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+			this.saveAsToolStripMenuItem.Text = "Save As...";
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+			this.exitToolStripMenuItem.Text = "Exit";
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutZMusicManagerToolStripMenuItem});
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+			this.helpToolStripMenuItem.Text = "Help";
+			// 
+			// aboutZMusicManagerToolStripMenuItem
+			// 
+			this.aboutZMusicManagerToolStripMenuItem.Name = "aboutZMusicManagerToolStripMenuItem";
+			this.aboutZMusicManagerToolStripMenuItem.Size = new System.Drawing.Size(239, 26);
+			this.aboutZMusicManagerToolStripMenuItem.Text = "About ZMusic Manager";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 37);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(49, 17);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Name:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 72);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(40, 17);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Bank";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 106);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(96, 17);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Music groups:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 144);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(91, 17);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Main volume:";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(12, 176);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(40, 17);
+			this.label5.TabIndex = 5;
+			this.label5.Text = "Type";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(115, 37);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(290, 22);
+			this.textBox1.TabIndex = 6;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(439, 491);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "ZMusic Manager";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem btnOpen;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem aboutZMusicManagerToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox1;
 	}
 }
 
--- ZMusicManager/Form1.cs
+++ ZMusicManager/Form1.cs
@@ -3,18 +3,45 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ZMusicManager
-{
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
+namespace ZMusicManager {
+	public partial class Form1 : Form {
+		public string FileName;
+
+		public Form1() {
 			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			OpenCurrentFile();
+		}
+
+		private void OpenCurrentFile() {
+			Text = "ZMusic Manager";
+			if (!string.IsNullOrEmpty(FileName)) Text = Path.GetFileName(FileName) + " - " + Text;
+
+			// TODO: GET ZIPPER LIBRARY
+			// TODO: FILL OUT FORM
+		}
+
+		private void btnOpen_Click(object sender, EventArgs e) {
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.InitialDirectory = "C:\\"; // <-- TODO: SAVE THE LAST USED FOLDER CROSS SESSION
+			ofd.Filter = "Ocarina of Time Radomizer Sound Files (*.ootrs)|*.ootrs";
+			ofd.RestoreDirectory = true;
+
+			// We show the open file dialog
+			DialogResult result = ofd.ShowDialog();
+
+			if (result == DialogResult.OK) {
+				FileName = ofd.FileName;
+				OpenCurrentFile();
+			}
 		}
 	}
 }
