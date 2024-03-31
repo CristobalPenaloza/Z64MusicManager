652a6f614bb915f4121f952c68d45a82ed03b690
Cristóbal Peñaloza <cristobal.tolin@gmail.com>
2024-03-30 22:27:01 -0300

    Open zip archive and meta files, and path persistence.

--- ZMusicManager/Form1.Designer.cs
+++ ZMusicManager/Form1.Designer.cs
@@ -41,8 +41,14 @@
 			this.label3 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
 			this.label5 = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.txtName = new System.Windows.Forms.TextBox();
+			this.cbxBank = new System.Windows.Forms.ComboBox();
+			this.txtMusicGroups = new System.Windows.Forms.TextBox();
+			this.cbxSequenceType = new System.Windows.Forms.ComboBox();
+			this.tbMainVolume = new System.Windows.Forms.TrackBar();
+			this.txtMainVolume = new System.Windows.Forms.TextBox();
 			this.menuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.tbMainVolume)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// menuStrip1
@@ -53,7 +59,7 @@
             this.helpToolStripMenuItem});
 			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(439, 28);
+			this.menuStrip1.Size = new System.Drawing.Size(444, 28);
 			this.menuStrip1.TabIndex = 0;
 			this.menuStrip1.Text = "menuStrip1";
 			// 
@@ -117,7 +123,7 @@
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 37);
+			this.label1.Location = new System.Drawing.Point(12, 40);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(49, 17);
 			this.label1.TabIndex = 1;
@@ -126,16 +132,16 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(12, 72);
+			this.label2.Location = new System.Drawing.Point(12, 75);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(40, 17);
+			this.label2.Size = new System.Drawing.Size(44, 17);
 			this.label2.TabIndex = 2;
-			this.label2.Text = "Bank";
+			this.label2.Text = "Bank:";
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(12, 106);
+			this.label3.Location = new System.Drawing.Point(12, 149);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(96, 17);
 			this.label3.TabIndex = 3;
@@ -144,7 +150,7 @@
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(12, 144);
+			this.label4.Location = new System.Drawing.Point(12, 187);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(91, 17);
 			this.label4.TabIndex = 4;
@@ -153,25 +159,80 @@
 			// label5
 			// 
 			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(12, 176);
+			this.label5.Location = new System.Drawing.Point(12, 112);
 			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(40, 17);
+			this.label5.Size = new System.Drawing.Size(99, 17);
 			this.label5.TabIndex = 5;
-			this.label5.Text = "Type";
+			this.label5.Text = "Sequece type:";
 			// 
-			// textBox1
+			// txtName
 			// 
-			this.textBox1.Location = new System.Drawing.Point(115, 37);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(290, 22);
-			this.textBox1.TabIndex = 6;
+			this.txtName.Location = new System.Drawing.Point(133, 37);
+			this.txtName.Name = "txtName";
+			this.txtName.Size = new System.Drawing.Size(293, 22);
+			this.txtName.TabIndex = 6;
+			// 
+			// cbxBank
+			// 
+			this.cbxBank.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbxBank.FormattingEnabled = true;
+			this.cbxBank.Items.AddRange(new object[] {
+            "0x03 Hyrule Field"});
+			this.cbxBank.Location = new System.Drawing.Point(133, 72);
+			this.cbxBank.Name = "cbxBank";
+			this.cbxBank.Size = new System.Drawing.Size(293, 24);
+			this.cbxBank.TabIndex = 7;
+			// 
+			// txtMusicGroups
+			// 
+			this.txtMusicGroups.Location = new System.Drawing.Point(133, 146);
+			this.txtMusicGroups.Name = "txtMusicGroups";
+			this.txtMusicGroups.Size = new System.Drawing.Size(293, 22);
+			this.txtMusicGroups.TabIndex = 8;
+			// 
+			// cbxSequenceType
+			// 
+			this.cbxSequenceType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbxSequenceType.ForeColor = System.Drawing.SystemColors.WindowText;
+			this.cbxSequenceType.FormattingEnabled = true;
+			this.cbxSequenceType.Items.AddRange(new object[] {
+            "Bgm",
+            "Fanfare"});
+			this.cbxSequenceType.Location = new System.Drawing.Point(133, 109);
+			this.cbxSequenceType.Name = "cbxSequenceType";
+			this.cbxSequenceType.Size = new System.Drawing.Size(293, 24);
+			this.cbxSequenceType.TabIndex = 9;
+			// 
+			// tbMainVolume
+			// 
+			this.tbMainVolume.Location = new System.Drawing.Point(133, 183);
+			this.tbMainVolume.Maximum = 127;
+			this.tbMainVolume.Name = "tbMainVolume";
+			this.tbMainVolume.Size = new System.Drawing.Size(253, 56);
+			this.tbMainVolume.TabIndex = 11;
+			this.tbMainVolume.Value = 88;
+			this.tbMainVolume.ValueChanged += new System.EventHandler(this.tbMainVolume_ValueChanged);
+			// 
+			// txtMainVolume
+			// 
+			this.txtMainVolume.Location = new System.Drawing.Point(392, 183);
+			this.txtMainVolume.Name = "txtMainVolume";
+			this.txtMainVolume.ReadOnly = true;
+			this.txtMainVolume.Size = new System.Drawing.Size(34, 22);
+			this.txtMainVolume.TabIndex = 12;
+			this.txtMainVolume.Text = "58";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(439, 491);
-			this.Controls.Add(this.textBox1);
+			this.ClientSize = new System.Drawing.Size(444, 491);
+			this.Controls.Add(this.txtMainVolume);
+			this.Controls.Add(this.tbMainVolume);
+			this.Controls.Add(this.cbxSequenceType);
+			this.Controls.Add(this.txtMusicGroups);
+			this.Controls.Add(this.cbxBank);
+			this.Controls.Add(this.txtName);
 			this.Controls.Add(this.label5);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.label3);
@@ -184,6 +245,7 @@
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.menuStrip1.ResumeLayout(false);
 			this.menuStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.tbMainVolume)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -205,7 +267,12 @@
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Label label5;
-		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox txtName;
+		private System.Windows.Forms.ComboBox cbxBank;
+		private System.Windows.Forms.TextBox txtMusicGroups;
+		private System.Windows.Forms.ComboBox cbxSequenceType;
+		private System.Windows.Forms.TrackBar tbMainVolume;
+		private System.Windows.Forms.TextBox txtMainVolume;
 	}
 }
 
--- ZMusicManager/Form1.cs
+++ ZMusicManager/Form1.cs
@@ -8,8 +8,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Compression;
 
-namespace ZMusicManager {
+namespace ZMusicManager{
 	public partial class Form1 : Form {
 		public string FileName;
 
@@ -18,20 +19,66 @@ namespace ZMusicManager {
 		}
 
 		private void Form1_Load(object sender, EventArgs e) {
+			cbxBank.DisplayMember = "Name";
+			cbxBank.ValueMember = "Id";
+			cbxBank.DataSource = Z64Bank.OoTBanks;
+
 			OpenCurrentFile();
 		}
 
 		private void OpenCurrentFile() {
 			Text = "ZMusic Manager";
-			if (!string.IsNullOrEmpty(FileName)) Text = Path.GetFileName(FileName) + " - " + Text;
+			if (!string.IsNullOrEmpty(FileName)) {
+				Text = Path.GetFileName(FileName) + " - " + Text;
 
-			// TODO: GET ZIPPER LIBRARY
-			// TODO: FILL OUT FORM
+				// We open the ootrs file as zip
+				using (ZipArchive archive = ZipFile.OpenRead(FileName)) {
+					foreach (ZipArchiveEntry entry in archive.Entries) {
+						string extension = Path.GetExtension(entry.Name).ToLower();
+
+						// Process the .meta file
+						if (extension == ".meta") {
+							using(var reader = new StreamReader(entry.Open())) {
+
+								// Read the .meta file line by line
+								string line;
+								int lineIndex = 0;
+								while ((line = reader.ReadLine()) != null) {
+
+									// Song name
+									if (lineIndex == 0) txtName.Text = line;
+
+									// Bank
+									else if (lineIndex == 1) cbxBank.SelectedItem = Z64Bank.OoTBanks.Where(b => b.Id == line).FirstOrDefault();
+
+									// Sequence type
+									else if (lineIndex == 2) cbxSequenceType.SelectedIndex = cbxSequenceType.FindStringExact(line);
+
+									// Music groups
+									else if (lineIndex == 3) txtMusicGroups.Text = line;
+
+									// TODO: CUSTOM SAMPLE STUFF...
+									lineIndex++;
+								}
+
+							}
+						}
+
+						// TODO: Process the .seq file
+						if(extension == ".seq") {
+
+							// TODO: GET THE VOLUME
+						}
+					}
+				}
+			}
+
+			// TODO: CLEAN FORM
 		}
 
 		private void btnOpen_Click(object sender, EventArgs e) {
 			OpenFileDialog ofd = new OpenFileDialog();
-			ofd.InitialDirectory = "C:\\"; // <-- TODO: SAVE THE LAST USED FOLDER CROSS SESSION
+			ofd.InitialDirectory = Properties.Settings.Default.LastPath ?? "C:\\";
 			ofd.Filter = "Ocarina of Time Radomizer Sound Files (*.ootrs)|*.ootrs";
 			ofd.RestoreDirectory = true;
 
@@ -39,9 +86,65 @@ namespace ZMusicManager {
 			DialogResult result = ofd.ShowDialog();
 
 			if (result == DialogResult.OK) {
+				// Save the filename path in a setting so we can persist it for the future...
 				FileName = ofd.FileName;
+				Properties.Settings.Default.LastPath = Path.GetDirectoryName(FileName);
+				Properties.Settings.Default.Save();
+
+				// We open the file!
 				OpenCurrentFile();
 			}
+		}
+
+		private void tbMainVolume_ValueChanged(object sender, EventArgs e) {
+			txtMainVolume.Text = tbMainVolume.Value.ToString("X");
+		}
+	}
+
+	public class Z64Bank {
+		public string Id;
+		public string Name;
+
+		public static List<Z64Bank> OoTBanks = new List<Z64Bank> {
+			new Z64Bank{ Id = "0x03", Name = "0x03 - Main Orchestra/Hyrule Field" },
+			new Z64Bank{ Id = "0x04", Name = "0x04 - Deku Tree" },
+			new Z64Bank{ Id = "0x05", Name = "0x05 - Market" },
+			new Z64Bank{ Id = "0x06", Name = "0x06 - Title Screen" },
+			new Z64Bank{ Id = "0x07", Name = "0x07 - Jabu Jabu's Belly" },
+			new Z64Bank{ Id = "0x08", Name = "0x08 - Kakariko Village (Guitar)" },
+			new Z64Bank{ Id = "0x09", Name = "0x09 - Fairy Fountain" },
+			new Z64Bank{ Id = "0x0A", Name = "0x0A - Fire Temple" },
+			new Z64Bank{ Id = "0x0B", Name = "0x0B - Dodongo's Cavern" },
+			new Z64Bank{ Id = "0x0C", Name = "0x0C - Forest Temple" },
+			new Z64Bank{ Id = "0x0D", Name = "0x0D - Lon Lon Ranch" },
+			new Z64Bank{ Id = "0x0E", Name = "0x0E - Goron City" },
+			new Z64Bank{ Id = "0x0F", Name = "0x0F - Kokiri Forest" },
+			new Z64Bank{ Id = "0x10", Name = "0x10 - Spirit Temple" },
+			new Z64Bank{ Id = "0x11", Name = "0x11 - Horse Race" },
+			new Z64Bank{ Id = "0x12", Name = "0x12 - Warp Songs" },
+			new Z64Bank{ Id = "0x13", Name = "0x13 - Goddess Cutscene" },
+			new Z64Bank{ Id = "0x14", Name = "0x14 - Shooting Gallery" },
+			new Z64Bank{ Id = "0x15", Name = "0x15 - Zora's Domain" },
+			new Z64Bank{ Id = "0x16", Name = "0x16 - Shop" },
+			new Z64Bank{ Id = "0x17", Name = "0x17 - Ice Cavern" },
+			new Z64Bank{ Id = "0x18", Name = "0x18 - Shadow Temple" },
+			new Z64Bank{ Id = "0x19", Name = "0x19 - Water Temple" },
+			new Z64Bank{ Id = "0x1A", Name = "0x1A - Piano (Unused)" },
+			new Z64Bank{ Id = "0x1B", Name = "0x1B - Gerudo Valley" },
+			new Z64Bank{ Id = "0x1C", Name = "0x1C - Lakeside Laboratory" },
+			new Z64Bank{ Id = "0x1D", Name = "0x1D - Kotake and Koume" },
+			new Z64Bank{ Id = "0x1E", Name = "0x1E - Ganon's Castle (Organ)" },
+			new Z64Bank{ Id = "0x1F", Name = "0x1F - Inside Ganon's Castle" },
+			new Z64Bank{ Id = "0x20", Name = "0x20 - Ganondorf Battle" },
+			new Z64Bank{ Id = "0x21", Name = "0x21 - Ending Sequence 1" },
+			new Z64Bank{ Id = "0x22", Name = "0x22 - Ending Sequence 2" },
+			new Z64Bank{ Id = "0x23", Name = "0x23 - Fanfares" },
+			new Z64Bank{ Id = "0x24", Name = "0x24 - Owl" },
+			new Z64Bank{ Id = "-", Name = "Custom Bank" },
+		};
+
+		public override string ToString() {
+			return Name;
 		}
 	}
 }
